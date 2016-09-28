114297202eee5b3b4503c3e0de70e5239b7a2d25
Luca Scalzotto <lucascalzotto@gmail.com>
2016-09-28 11:20:12 +0200

    Fix indents

--- kickstart.sh
+++ kickstart.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Check if we are running as root.
-if [ "$EUID" -ne 0 ]; then
-  echo "This script needs to be executed as root to work properly."
-  exit 1
+if [ "$EUID" -ne 0 ]
+then
+	echo "This script needs to be executed as root to work properly."
+	exit 1
 fi
 
 # Execute the helper scripts.
