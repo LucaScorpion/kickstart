15d74492667236b4d0fbb4d0381947ad28b81ebd
Luca Scalzotto <lucascalzotto@gmail.com>
2016-10-10 10:11:02 +0200

    Remove java-package from packages.sh

--- packages.sh
+++ packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # The packages to install.
-packages=(sudo git curl screenfetch filezilla gimp bless qbittorrent java-package fonts-powerline)
+packages=(sudo git curl screenfetch filezilla gimp bless qbittorrent fonts-powerline)
 
 # Set the sources.list file to contain all the standard repos.
 printf "Updating sources.list (a backup will be created).\n"
