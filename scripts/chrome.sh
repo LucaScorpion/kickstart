cfd6726c6980240510a31c361e003acc077ae0ed
Luca Scalzotto <luca@scorpiac.com>
2017-03-10 13:25:36 +0100

    Add check if Chrome is already installed

--- scripts/chrome.sh
+++ scripts/chrome.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+# Check if Chrome is already installed.
+if [ $(which google-chrome) ]
+then
+	printf "Google Chrome is already installed, skipping.\n"
+	exit
+fi
+
 # Create the source entry.
 printf "Creating source entry:\n"
 entry="deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main"
 echo $entry | sudo tee /etc/apt/sources.list.d/google-chrome.list
 
-# Update and install chrome.
+# Update and install Chrome.
 printf "Updating package list.\n"
 sudo apt-get update > /dev/null
 printf "Installing Google Chrome.\n"
