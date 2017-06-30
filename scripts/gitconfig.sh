961ffc1bca02d4be8f396e81451df0ec67383ba9
Luca Scalzotto <luca@scorpiac.com>
2017-06-30 09:38:56 +0200

    Add user config to gitconfig script

--- scripts/gitconfig.sh
+++ scripts/gitconfig.sh
@@ -4,3 +4,23 @@
 printf "Setting git configuration options.\n"
 git config --global remote.origin.prune true
 git config --global credential.helper cache
+
+# Set user config.
+printf "Checking git user configuration.\n"
+
+name=$(git config --global user.name)
+email=$(git config --global user.email)
+
+if [ -z "$name" ]
+then
+	printf "Enter your name: "
+	read -e name
+	git config --global user.name "$name"
+fi
+
+if [ -z "$email" ]
+then
+	printf "Enter your e-mail address: "
+	read -e email
+	git config --global user.email "$email"
+fi
