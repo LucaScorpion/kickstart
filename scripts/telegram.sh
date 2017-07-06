18ce9b52a8fa7348933649c2b35d0c08db719bee
Luca Scalzotto <luca@scorpiac.com>
2017-07-06 14:45:32 +0200

    Add telegram install script

---
+++ scripts/telegram.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Skip in fast mode.
+if [ "$KICKSTART_FAST" = true ]
+then
+	printf "Skipping Telegram.\n"
+	exit
+fi
+
+# Check if Telegram is already installed.
+if [ -d "/opt/Telegra" ]
+then
+	printf "Telegram is already installed, skipping.\n"
+	exit
+fi
+
+# The link to download Telegram from.
+telegramSite="https://telegram.org/dl/desktop/linux"
+
+# Download the archive.
+printf "Downloading Telegram desktop.\n"
+tmpTar="/tmp/telegram.tar.xz"
+curl -L# $telegramSite > "$tmpTar"
+
+# Extract the archive.
+printf "Installing Telegram.\n"
+sudo tar xf "$tmpTar" -C "/opt"
+
+# Clean up.
+printf "Cleaning up.\n"
+rm "$tmpTar"
