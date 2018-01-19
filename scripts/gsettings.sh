46f8fab4003f6ddd1e63c1f233b1b34a4029645c
Luca Scalzotto <luca@scorpiac.com>
2018-01-19 11:10:31 +0100

    Add show hidden files to gsettings

--- scripts/gsettings.sh
+++ scripts/gsettings.sh
@@ -31,5 +31,6 @@ gsettings set org.gnome.nautilus.preferences search-view list-view
 # Gtk.
 printf "%s Gtk" "-"
 gsettings set org.gtk.Settings.FileChooser sort-directories-first true
+gsettings set org.gtk.Settings.FileChooser show-hidden true
 
 printf "\n"
