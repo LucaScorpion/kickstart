435c9347ee12abfc377b25ce5ad25c5c50ec872c
Luca Scalzotto <luca@scorpiac.com>
2018-05-17 07:51:16 +0200

    Remove gtksettings script

--- scripts/gtksettings.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Create the gtk settings file.
-if [ ! -f "$HOME/.config/gtk-4.0/settings.ini" ]
-then
-	printf "Creating gtk settings file.\n"
-	printf "[Settings]\ngtk-application-prefer-dark-theme=1\n" > "$HOME/.config/gtk-4.0/settings.ini"
-fi
